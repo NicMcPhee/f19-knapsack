e1858809a591b4c464bc05e47b11dac64350f204
Nic McPhee <mcphee@morris.umn.edu>
2019-10-08 15:22:06 -0500

    Get working GA

--- src/knapsack/knapsack.clj
+++ src/knapsack/knapsack.clj
@@ -22,37 +22,98 @@
   [num-items]
   (repeatedly num-items #(rand-int 2)))
 
+(defn make-individual
+  [generation bits]
+  {:generation generation
+   :bits bits})
+
 (defn make-population
   [num-individuals num-items]
   (repeatedly num-individuals
-              #(make-random-solution num-items)))
+              #(make-individual 0 (make-random-solution num-items))))
 
 (defn summarize-individual
   [problem individual]
   (let [items (:items problem)
         values (map :value items)
         weights (map :weight items)
-        total-value (reduce + (map * values individual))
-        total-weight (reduce + (map * weights individual))]
-    {:total-value total-value
-     :total-weight total-weight}))
+        total-value (reduce + (map * values (:bits individual)))
+        total-weight (reduce + (map * weights (:bits individual)))]
+    (merge individual
+           {:total-value total-value
+            :total-weight total-weight})))
 
-(defn ethan-hamers-fitness
+(defn summarize-population
+  [problem population]
+  (map (partial summarize-individual problem) population))
+
+(defn ethan-hamers-quality
   [problem individual]
-  (let [summary (summarize-individual problem individual)]
-    (/ (:total-value summary)
-       (+ 1 (Math/abs (- (:total-weight summary)
-                         (:max-capacity problem)))))))
+  (/ (:total-value individual)
+     (+ 1 (Math/abs (- (:total-weight individual)
+                       (:max-capacity problem))))))
 
+(defn harsh-quality
+  [problem individual]
+  (if (> (:total-weight individual)
+         (:max-capacity problem))
+    0
+    (:total-value individual)))
+
+(defn ramped-quality
+  [problem individual]
+  (if (> (:total-weight individual)
+         (:max-capacity problem))
+    (- (:total-weight individual))
+    (:total-value individual)))
+
+(defn evaluate-population
+  [evaluator problem population]
+  (map #(assoc % :quality (evaluator problem %)) 
+       (summarize-population problem population)))
+
+; Assumes popl has been evaluated, i.e., every individual has a
+; `:quality` field.
 (defn tournament-selection
-  [problem tournament-size popl]
+  [tournament-size popl]
   (let [tournament (take tournament-size (shuffle popl))
-        fitnesses (map (partial ethan-hamers-fitness problem) tournament)
-        pairs (map vector tournament fitnesses)
-        max-fitness (apply max fitnesses)]
-    (first (first (filter #(= max-fitness (second %)) pairs)))))
+        qualities (map :quality tournament)
+        max-quality (apply max qualities)]
+    (first (filter #(= max-quality (:quality %)) tournament))))
 
 (defn select-parents
-  [problem tournament-size popl]
+  [tournament-size popl]
+  (println (* 1.0 (apply max (map :quality popl))))
   (let [pop-size (count popl)]
-    (repeatedly pop-size #(tournament-selection problem tournament-size popl))))
+    (repeatedly pop-size #(tournament-selection tournament-size popl))))
+
+(defn make-child-copy
+  [parent]
+  (assoc parent :generation (inc (:generation parent))))
+
+(defn mutate-bit
+  [mutation-rate bit]
+  (if (< (rand) mutation-rate)
+    (- 1 bit)
+    bit))
+
+(defn mutate-bits
+  [mutation-rate bits]
+  (map (partial mutate-bit mutation-rate) bits))
+
+(defn make-child
+  [mutation-rate parent]
+  {:generation (inc (:generation parent))
+   :bits (mutate-bits mutation-rate (:bits parent))})
+
+(defn next-generation
+  [evaluator prob tournament-size popl]
+  (map (partial make-child (/ 1 (count (:items prob))))
+       (select-parents tournament-size
+                       (evaluate-population evaluator prob popl))))
+
+; (def prob (ks/make-random-problem 50 10 25 25))
+; (def popl (ks/make-population 100 10))
+; (ks/evaluate-population ks/ramped-quality prob (nth (iterate (partial ks/next-generation ks/ramped-quality prob 2) popl) 100))
+; (nth (iterate (partial ks/next-generation prob 2) popl) 10)
+
